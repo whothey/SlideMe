4af2b708be25a171edcca898ccd75d1228fc5b16
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2014-11-28 20:17:31 -0200

    Adding a Splash Screen with gamename

---
+++ SlideMe/src/com/uffs/slideme/MainState.java
@@ -0,0 +1,26 @@
+package com.uffs.slideme;
+
+import org.flixel.FlxG;
+import org.flixel.FlxState;
+import org.flixel.FlxText;
+
+public class MainState extends FlxState 
+{
+	private FlxText logo;
+	private float _count;
+	
+	@Override
+	public void create()
+	{
+		logo = new FlxText(0, 0, 200, "SlideMe");
+		logo.setSize(20);
+		logo.setAlignment("center");
+		logo.x = FlxG.width / 2 - logo.width / 2;
+		logo.y = FlxG.height / 2 - logo.height / 2;
+		// logo.setAlpha(0); // Transparent
+		
+		FlxG.fade(0x000000, 3);
+		
+		add(logo);
+	}
+}
