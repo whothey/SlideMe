2cf35c74c86b8bcce1179fa41fe4d93c2616ad6f
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2014-11-30 23:35:37 -0200

    Add Fumiko
    Fumiko is our player class/sprites

---
+++ SlideMe/src/com/uffs/slideme/Fumiko.java
@@ -0,0 +1,41 @@
+package com.uffs.slideme;
+
+import org.flixel.FlxButton;
+import org.flixel.FlxG;
+import org.flixel.FlxSprite;
+import org.flixel.ui.FlxVirtualPad;
+
+public class Fumiko extends FlxSprite
+{
+	protected String ImgFumiko = "player.png";
+	protected FlxVirtualPad _pad;
+	
+	public Fumiko(int x, int y)
+	{
+		super(x, y);
+		loadGraphic(ImgFumiko, true, true, 312, 24);
+		
+		// Animations
+		addAnimation("idle", new int[]{0});
+		addAnimation("walking", new int[]{0, 1, 2});
+		addAnimation("running", new int[]{3, 4, 5});
+		addAnimation("jumping", new int[]{9, 10, 11, 12});
+		
+		play("idle");
+	}
+	
+	public void update()
+	{
+		super.update();
+		
+		// Movement
+		acceleration.x = 0;
+		/*if (FlxG.keys.RIGHT || _pad.buttonRight.status == FlxButton.PRESSED) {
+			setFacing(RIGHT);
+			acceleration.x += drag.x;
+		} else if (FlxG.keys.LEFT || _pad.buttonLeft.status == FlxButton.PRESSED) {
+			setFacing(LEFT);
+			acceleration.x -= drag.x;
+		}*/
+	}
+}
